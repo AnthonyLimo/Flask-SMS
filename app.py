8c60397721284e10da54d7e3088352ae1faeabc4
Anthony Kiplimo <anthonylimo90@gmail.com>
2018-11-30 16:59:37 +0300

    Making slight changes

--- app.py
+++ app.py
@@ -5,6 +5,25 @@ import africastalking
 
 app = Flask(__name__)
 
-@app.route("/")
+username = "sandbox"
+api_key = "f6c6e1d0686fddddb375d47c8bcda0dc80713f214ce26b912567ab156ae7feee"
+africastalking.initialize(username,api_key)
+
+@app.route("/", methods=["GET","POST"])
 def main():
+    if request.method == "POST":
+        sms_message = request.form["smsMessage"]
+        phone_number = request.form["phoneNumber"]
+
+        print(sms_message)
+        print(phone_number)
+
+        response = sms.send(sms_message,[phone_number])
+        print(response)
+
+        
+
     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
