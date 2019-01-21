5c46e0c1bed616a9ab7c0f7a8f79409a0a1ac518
Anthony Kiplimo <anthonylimo90@gmail.com>
2019-01-21 11:32:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 
+import os
+
 import africastalking
 
 
@@ -26,4 +28,4 @@ def main():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=os.environ.get('PORT'),debug=True)
